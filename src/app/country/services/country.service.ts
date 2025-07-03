a17ab3c4c9795a52cadcb8d5b09d6a93bff803c6
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-07-02 21:47:30 -0400

    cache

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { CountryMapper } from '../mappers/country.mapper';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map, Observable, catchError, throwError, delay } from 'rxjs';
+import { map, Observable, catchError, throwError, of, tap, delay } from 'rxjs';
 import type { Country } from '../interfaces/country.interface';
 import type { RESTCountry } from '../interfaces/rest-countries.interface';
 
@@ -12,14 +12,21 @@ const API_URL = 'https://restcountries.com/v3.1';
 })
 export class CountryService {
   private http = inject(HttpClient);
+  private queryCacheCapital = new Map<string, Country[]>();
+  private queryCacheCountry = new Map<string, Country[]>();
 
   searchByCapital(query: string): Observable<Country[]> {
     const URL = `${API_URL}/capital/${query}`;
     query = query.toLowerCase();
 
+    if (this.queryCacheCapital.has(query)) {
+      return of(this.queryCacheCapital.get(query) ?? []);
+    }
+
     return this.http.get<RESTCountry[]>(URL).pipe(
       map((resp) => CountryMapper.mapRestCountryToCountryArray(resp)),
-      // delay(2000),
+      tap((countries) => this.queryCacheCapital.set(query, countries)),
+      delay(2000),
       catchError((error) => {
         console.log('Error fetching ', error);
 
@@ -34,9 +41,16 @@ export class CountryService {
     const URL = `${API_URL}/name/${query}`;
     query = query.toLowerCase();
 
+    if (this.queryCacheCountry.has(query)) {
+      return of(this.queryCacheCountry.get(query) ?? []);
+    }
+
+    console.log('LLegando al servidor por', query);
+
     return this.http.get<RESTCountry[]>(URL).pipe(
       map((resp) => CountryMapper.mapRestCountryToCountryArray(resp)),
-      // delay(2000),
+      tap((countries) => this.queryCacheCountry.set(query, countries)),
+      delay(2000),
       catchError((error) => {
         console.log('Error fetching ', error);
 
@@ -53,7 +67,7 @@ export class CountryService {
     return this.http.get<RESTCountry[]>(URL).pipe(
       map((resp) => CountryMapper.mapRestCountryToCountryArray(resp)),
       map((countries) => countries.at(0)),
-      // delay(2000),
+      delay(2000),
       catchError((error) => {
         console.log('Error fetching ', error);
 
