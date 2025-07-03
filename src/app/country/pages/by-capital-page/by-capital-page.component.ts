fe63958d6f3bf6d93369c526af1cafdaa3899816
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-07-03 10:59:10 -0400

    homework

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, inject, linkedSignal, signal } from '@angular/core';
 import { CountryService } from './../../services/country.service';
 import { ListComponent } from '../../components/list/list.component';
 import { of } from 'rxjs';
@@ -12,12 +13,24 @@ import { SearchInputComponent } from '../../components/search-input/search-input
 })
 export class ByCapitalPageComponent {
   countryService = inject(CountryService);
-  query = signal('');
+
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+
+  queryParam = this.activatedRoute.snapshot.queryParamMap.get('query') ?? '';
+
+  query = linkedSignal(() => this.queryParam);
 
   capitalResource = rxResource({
     request: () => ({ query: this.query() }),
     loader: ({ request }) => {
       if (!request.query) return of([]);
+
+      this.router.navigate(['/country/by-capital'], {
+        queryParams: {
+          query: request.query,
+        },
+      });
 
       return this.countryService.searchByCapital(request.query);
     },
